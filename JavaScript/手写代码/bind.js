60de401bc0a231b511cfc30fc59d688e3af6df40
hpysirius <hpysirius@gmail.com>
2019-06-19 10:02:48 +0800

    feat: bind

--- JavaScript/手写代码/bind.js
+++ JavaScript/手写代码/bind.js
@@ -12,11 +12,39 @@ Function.prototype.bind2 = function (...args) {
   const self = this;
   const context = args[0];
   const args1 = args.slice(1);
-  function bindFunc(...args2) {
-    return self.apply(this instanceof bindFunc ? this : context, [...args1, ...args2]);
+  function fBound(...args2) {
+    return self.apply(this instanceof fBound ? this : context, [...args1, ...args2]);
   }
-  function F() {}
-  F.prototype = this.prototype;
-  bindFunc.prototype = new F();
-  return bindFunc;
+  function fNOP() {}
+  // 维护原型关系
+  if (this.prototype) {
+    fNOP.prototype = this.prototype;
+  }
+  // 下行的代码使fBound.prototype是fNOP的实例,因此
+  // 返回的fBound若作为new的构造函数,new生成的新对象作为this传入fBound,新对象的__proto__就是fNOP的实例
+  fBound.prototype = new fNOP();
+  return fBound;
 };
+
+const foo = {
+  value: 1
+}
+;
+function bar(name, age) {
+  this.name = name;
+  this.age = age;
+  console.log(this.value);
+}
+bar.prototype.friend = 'kevin';
+bar.prototype.getName = function () {
+  console.log(this.name);
+};
+const NewBar = bar.bind(foo, 'hpy');
+// NewBar();
+const obj = new NewBar(20);
+console.log(obj);
+console.log(obj.constructor);
+
+// NewBar.prototype.friend = 'aaa'; // 修改绑定函数原型属性不会影响原函数的原型属性
+// console.log(bar.prototype.friend);
+// bind 当作构造器，bind的this就失效了，所以当构造器使用，没啥意义了。
