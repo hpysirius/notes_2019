9e24b590531aba5777d1941169de2dcf4e1c7a88
hpysirius <hpysirius@gmail.com>
2019-07-01 01:00:45 +0800

    feat: promise

--- JS/ES6_7/Promise/index.js
+++ JS/ES6_7/Promise/index.js
@@ -1,9 +1,24 @@
 console.log(Promise);
-new Promise((resolve) => {
+new Promise((resolve, reject) => {
   console.log(1);
   resolve(3);
-  Promise.resolve().then(() => console.log(4));
+  // reject(4);
+  // Promise.resolve()
+  Promise.resolve().then(
+    () => {
+      Promise.resolve().then(() => {
+        console.log('8');
+        Promise.resolve().then(() => {
+          console.log('9');
+        });
+        console.log('7');
+      });
+      console.log('6');
+    }
+  );
 }).then((num) => {
+  console.log(num);
+}, (num) => {
   console.log(num);
 });
 console.log(2);
---
+++ JS/浏览器/EventLoop/MutationObserver/README.md
@@ -0,0 +1,2 @@
+### MutationObserver
+Mutation Observer API 用来监视 DOM 变动。DOM 的任何变动，比如节点的增减、属性的变动、文本内容的变动，这个 API 都可以得到通知。
