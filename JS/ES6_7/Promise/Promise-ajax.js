28ccf88c359cfa6ad68bcc5f34afd41721e7b729
hpysirius <hpysirius@gmail.com>
2019-07-01 09:48:43 +0800

    feat: xmlHttpRequest

---
+++ JS/ES6_7/Promise/Promise-ajax.js
@@ -0,0 +1,26 @@
+const getJSON = (url) => {
+  const promise = new Promise((resolve, reject) => {
+    const handler = function () {
+      if (this.readyState !== 4) return;
+      if (this.status === 200) {
+        resolve(this.response);
+      } else {
+        reject(new Error(this.statusText));
+      }
+    };
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onreadystatechange = handler;
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.send();
+  });
+  return promise;
+};
+
+getJSON('./p.json').then((json) => {
+  console.log(json);
+  console.log(`Contents: ${json}`);
+}, (error) => {
+  console.error('出错了', error);
+});
