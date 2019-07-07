d98b76787d9c4e6733ed12dc7134294b8b3d44c0
hui.huang <hui.huang@56qq.com>
2019-07-07 23:39:01 +0800

    feat: promisethen

--- JS/ES6_7/Promise/执行/1.promisethen.js
+++ JS/ES6_7/Promise/执行/1.promisethen.js
@@ -1,47 +1,58 @@
-Promise.resolve()
-    .then(
-        () => {
-            console.log('promise 1')
-            // cb1
-            Promise.reject().then(
-                () => {},
-                () => {
-                    console.log('promise 2')
-                    // cb4
-                    Promise.resolve().then(() => {
-                        console.log('promise 3')
-                    })
-                });
-            return 1;
-        })
-    .then(
-        // cb2
-        () => {
-            Promise.resolve().then(() => {
-                console.log('promise 4')
-            })
-            console.log('promise 5');
-        }
-    )
-    .then(
-        // cb3
-        () => {
-            console.log('promise 8')
-        }
-    )
-/**
- * 1. promise 1,
- * 2. 状态改变，cb1进入queue
- * 3. cb2进入自己的queue
- * 4. cb3进入自己的promise的queue
- * 5. 执行cb1，把
- *
- * end： [cb1, cb2, ]
- */
+// Promise.resolve()
+//     .then(
+//         // cb1
+//         () => {
+//             console.log('promise 1')
+//             Promise.resolve().then(
+//                 // cb2
+//                 () => {
+//                     console.log('promise 2')
+//                     Promise.resolve().then(
+//                         // cb3
+//                         () => { console.log('promise 3') })
+//                 })
+//                 .then(
+//                     // cb4
+//                     () => {
+//                         console.log('promise 4')
+//                         Promise.resolve().then(
+//                             // cb5
+//                             () => { console.log('promise 5') })
+//                     })
+//         })
+//     .then(
+//         // cb6
+//         () => {
+//             console.log('promise 7');
+//             Promise.resolve().then(
+//                 // cb7
+//                 () => { console.log('promise 6') })
+//         }
+//     )
+//     .then(
+//         // cb8
+//         () => {
+//             console.log('promise 8')
+//         }
+//     )
 
-promise 1
-VM40:9 promise 2
-VM40:23 promise 5
-VM40:12 promise 3
-VM40:21 promise 4
-VM40:29 promise 8
+
+Promise.resolve()
+    .then(() => {
+        console.log('promise 1');
+        return Promise.resolve().then(
+            () => {
+                console.log('promise 2');
+                return Promise.resolve().then(
+                    () => {
+                        console.log('promise 3');
+                    }
+                )
+            }
+        )
+
+    })
+    .then(
+        () => { console.log('promise 4') }
+    )
+
