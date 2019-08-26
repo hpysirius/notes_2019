bc920743c8038b3fe781740f35f71067e8b8476c
hui.huang <hui.huang@56qq.com>
2019-08-26 14:11:57 +0800

    feat: reduce 实现map

--- 手写代码/9.reduce实现map.js
+++ 手写代码/9.reduce实现map.js
@@ -1,6 +1,5 @@
 Array.prototype.map1 = function (cb) {
-  const arr = this;
-  return arr.reduce((res, cur, i) => res.concat(cb(cur, i, arr)), []);
+  return this.reduce((res, cur, i) => res.concat(cb(cur, i, this)), []);
 };
 const m = [1, 2, 3, 4, 54].map1((v, i, arr) => v + v);
 console.log(m);
