c69acdd49e5e5489a84fa77ecd089fe976bf8e54
hui.huang <hui.huang@56qq.com>
2019-09-08 14:49:50 +0800

    feat: 手写jsonp

---
+++ 手写代码/13.jsonp.js
@@ -0,0 +1,10 @@
+function jsonp(url, jsonpCallback, success) {
+    const script = document.createElement('script')
+    script.src = url
+    script.async = true
+    script.type = 'text/javascript'
+    window[jsonpCallback] = function (data) {
+        success && success(data)
+    }
+    document.body.appendChild(script)
+}
