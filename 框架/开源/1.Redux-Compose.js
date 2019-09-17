a11703778095e3c36d6d3f06c883f6fda86375e4
hui.huang <hui.huang@56qq.com>
2019-09-17 15:39:23 +0800

    feat: 1.Redux-Compose

---
+++ 框架/开源/1.Redux-Compose.js
@@ -0,0 +1,31 @@
+
+// https://github.com/reduxjs/redux/blob/master/src/compose.ts
+const compose = (...funcs) => {
+    if(funcs.length === 0) {
+        return arg => arg;
+    }
+    if(funcs.length === 1) {
+        return funcs[0];
+    }
+    return funcs.reduce((a, b) => (...args) => a(b(...args)));
+}
+
+const a = () => console.log('a');
+const b = () => console.log('b');
+
+compose(a, b)();
+
+
+
+// export default function compose(...funcs: Function[]) {
+//     if (funcs.length === 0) {
+//       // infer the argument type so it is usable in inference down the line
+//       return <T>(arg: T) => arg
+//     }
+  
+//     if (funcs.length === 1) {
+//       return funcs[0]
+//     }
+  
+//     return funcs.reduce((a, b) => (...args: any) => a(b(...args)))
+//   }
---
+++ 框架/开源/REAEME.md
@@ -0,0 +1,9 @@
+### 开源中优秀的代码
+
+
+
+
+
+
+参考文章
+1. [你在阅读js开源项目源码的时候见过哪些高级写法？](https://www.zhihu.com/question/334043737?utm_source=wechat_session&utm_medium=social&utm_oi=27538105565184)
