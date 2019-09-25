2fd6f1d8fb84ffa8c0b6bc6750c0bccfe5a40b71
hui.huang <huangh2@knownsec.com>
2019-09-25 11:34:59 +0800

    feat: test

---
+++ Node/module/child_process/1.demo.js
@@ -0,0 +1,15 @@
+const { spawn } = require('child_process');
+const ls = spawn('ls', ['-lh', '/usr']);
+
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+    console.log(`子进程退出，使用退出码 ${code}`);
+});
+
